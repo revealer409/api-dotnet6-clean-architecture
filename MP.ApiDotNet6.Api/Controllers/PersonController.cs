deabcb6e597a67b4cd2b381bab97431f77c0d3ae
Vitor <vitor.fgaletti@gmail.com>
2023-05-04 10:43:54 -0300

    feat(person): Criando o controller de pessoa

---
+++ MP.ApiDotNet6.Api/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MP.Api.DotNet6.Application.DTOs;
+using MP.Api.DotNet6.Application.Services.Interfaces;
+
+namespace MP.ApiDotNet6.Api.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase {
+
+        private readonly IPersonService _personService;
+
+        public PersonController(IPersonService personService) {
+            _personService = personService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] PersonDTO personDTO) {
+            var result = await _personService.CreateAsync(personDTO);
+            
+            if(result.IsSuccess) {
+                return Ok(result);
+            }
+
+            return BadRequest(result); 
+        }
+    }
+}
