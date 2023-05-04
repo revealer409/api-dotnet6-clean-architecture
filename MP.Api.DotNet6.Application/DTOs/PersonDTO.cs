c55f4a56e80d7121e38a8b0c4251e1a540ae441a
Vitor <vitor.fgaletti@gmail.com>
2023-05-03 22:01:36 -0300

    feat(person): Criando a DTO de pessoa e suas validações

---
+++ MP.Api.DotNet6.Application/DTOs/PersonDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MP.Api.DotNet6.Application.DTOs {
+    public class PersonDTO {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Document { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ MP.Api.DotNet6.Application/DTOs/Validations/PersonDTOValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace MP.Api.DotNet6.Application.DTOs.Validations {
+    public class PersonDTOValidator : AbstractValidator<PersonDTO> {
+        public PersonDTOValidator() {
+
+            RuleFor(x => x.Document)
+                 .NotEmpty()
+                 .NotNull()
+                 .WithMessage("Document deve ser informado!");
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Name deve ser informado!");
+
+            RuleFor(x => x.Phone)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Phone deve ser informado!");
+        }
+    }
+}
