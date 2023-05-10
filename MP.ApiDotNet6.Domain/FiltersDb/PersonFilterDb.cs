4e8aa997fc7393a1ee662c0aea64fcc18b514e5d
Vitor <vitor.fgaletti@gmail.com>
2023-05-10 14:51:05 -0300

    feat: Paginação usando o Entity Framework Core (parte 1);

---
+++ MP.ApiDotNet6.Domain/FiltersDb/PersonFilterDb.cs
@@ -0,0 +1,9 @@
+﻿using MP.ApiDotNet6.Domain.Repositories;
+
+namespace MP.ApiDotNet6.Domain.FiltersDb {
+    public class PersonFilterDb : PageBaseRequest{
+    
+     public string Name { get; set; }
+
+    }
+}
