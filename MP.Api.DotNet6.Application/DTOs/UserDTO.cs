b4ba3020353f9532d50d1c07793709fb1739137b
Vitor <vitor.fgaletti@gmail.com>
2023-05-11 16:11:41 -0300

    feat: Criando a classe service para o token;

---
+++ MP.Api.DotNet6.Application/DTOs/UserDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace MP.Api.DotNet6.Application.DTOs {
+    public class UserDTO {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ MP.Api.DotNet6.Application/Services/Interfaces/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using MP.Api.DotNet6.Application.DTOs;
+
+namespace MP.Api.DotNet6.Application.Services.Interfaces {
+    public interface IUserService {
+
+        Task<ResultService<dynamic>> GenerateTokenAsync(UserDTO userDTO);
+
+    }
+}
