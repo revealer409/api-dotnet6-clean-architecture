e6e7c0bcd861db2f7f5bb9bc45a9bc1e531c4751
Vitor <vitor.fgaletti@gmail.com>
2023-05-11 09:22:51 -0300

    feat: Criando a tabela e a entidade para a autenticacao;

---
+++ MP.ApiDotNet6.Domain/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using MP.ApiDotNet6.Domain.Validations;
+
+namespace MP.ApiDotNet6.Domain.Entities {
+    public class User {
+        public int Id { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        public User(string email, string password) {
+            Validation(email, password);
+        }
+
+        public User(int id, string email, string password) {
+            DomainValidationException.When(id <= 0, "Id deve ser informado");
+            Id = id;
+            Validation(email, password);
+        }
+
+        private void Validation(string email, string password) {
+            DomainValidationException.When(string.IsNullOrEmpty(email), "Email deve ser informado");
+            DomainValidationException.When(string.IsNullOrEmpty(password), "Password deve ser informada");
+
+            Email = email;
+            Password = password;
+        }
+    }
+}
