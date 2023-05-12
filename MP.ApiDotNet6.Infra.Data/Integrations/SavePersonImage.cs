79fbb3d4c7031146461d12bd9fd8416bc2c4e7c1
Vitor <vitor.fgaletti@gmail.com>
2023-05-12 15:55:48 -0300

    feat: Salvando imagem no banco de dados (parte 5) ;

---
+++ MP.ApiDotNet6.Infra.Data/Integrations/SavePersonImage.cs
@@ -0,0 +1,29 @@
+﻿using MP.ApiDotNet6.Domain.Integrations;
+
+namespace MP.ApiDotNet6.Infra.Data.Integrations {
+    public class SavePersonImage : ISavePersonImage {
+
+        private readonly string _filePath;
+
+        public SavePersonImage() {
+            _filePath = "/Users/VitorAugustoGaletiFr/Desktop";
+        }
+
+        public string Save(string imageBase64) {
+            var fileExt = imageBase64.Substring(imageBase64.IndexOf("/") + 1, imageBase64.IndexOf(":") - imageBase64.IndexOf("/") - 1); // png ou jpge
+
+            var base64Code = imageBase64.Substring(imageBase64.IndexOf(",") + 1); // base64
+
+            var imgBytes = Convert.FromBase64String(base64Code);
+
+            var fileName = Guid.NewGuid().ToString() + "." + fileExt;
+
+            using (var imageFile = new FileStream(_filePath + "/" + fileName, FileMode.Create)) {
+                imageFile.Write(imgBytes, 0, imgBytes.Length);
+                imageFile.Flush();
+            }
+
+            return _filePath + "/" + fileName;
+        }
+    }
+}
